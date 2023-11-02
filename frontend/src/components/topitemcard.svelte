378219a8c9070b112f6d3d0dbe4144b0c65357da
tonydawhale <tonydawhale@gmail.com>
2023-11-02 13:01:59 -0700

    Add assets host

--- frontend/src/components/topitemcard.svelte
+++ frontend/src/components/topitemcard.svelte
@@ -2,12 +2,19 @@
 	import type { BazaarTopItem } from "$types";
 
     export let item: BazaarTopItem;
+    let baseItemImageUrl = `https://sb.tonydoes.dev/assets`
+
+    let itemImageUrl: string;
+
+    if (item._id.toLowerCase().startsWith("enchantment_")) itemImageUrl = `${baseItemImageUrl}/item/ENCHANTED_BOOK`
+    else if (item._id.toLowerCase().startsWith("essence_")) itemImageUrl = `${baseItemImageUrl}/essence/${item._id.toLowerCase().replace("essence_", "")}`
+    else itemImageUrl = `${baseItemImageUrl}/item/${item._id}`
 </script>
 
 
 <a href={`/product/${item._id}`} class="rounded-lg border-theme-500 border-2 p-[16px] min-w-full hover:scale-[1.02] select-none">
     <div class="flex flex-row items-center gap-[20px] mb-4">
-        <img src={`https://sky.shiiyu.moe/item/${item._id}`} class="w-[50px] h-[50px] rounded-lg" alt={item.display_name}/>
+        <img src={itemImageUrl} class="w-[50px] h-[50px] rounded-lg" alt={item.display_name}/>
         <h3 class="font-bold text-xl">{item.display_name}</h3>
     </div>
     <div class="grid sm:grid-cols-1 md:grid-cols-2 justify-center gap-[20px]">
